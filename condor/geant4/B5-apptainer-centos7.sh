fd52241a7fa778f53e22bf7c1bb4a9f3d67cc7fd
Go Iwai <go.iwai@kek.jp>
2025-07-23 16:28:52 +0900

    fix: remove commented verbose run command from B5-apptainer script

--- condor/geant4/B5-apptainer-centos7.sh
+++ condor/geant4/B5-apptainer-centos7.sh
@@ -1,6 +1,3 @@
 mkdir -p $WORKING_DIR || exit 1
 apptainer run --bind $WORKING_DIR:/workspace --bind $PWD --bind /cvmfs /cvmfs/unpacked.cern.ch/gitlab-registry.cern.ch/sft/docker/centos7:latest /bin/bash $PWD/B5-multiple-job.sh
-
-# apptainer --verbose run --cleanenv --bind /cvmfs /cvmfs/unpacked.cern.ch/gitlab-registry.cern.ch/sft/docker/centos7:latest cat /etc/redhat-release
-
 exit 0
