c988b3cc9031a0f5369733dea356bbdc425edfe3
Go Iwai <go.iwai@kek.jp>
2026-07-21 16:46:13 +0900

    fix: update create-input.sh to change block size from 16MB to 64KB for consistency across LRMS

--- lsf/dag-jobs/create-input.sh
+++ lsf/dag-jobs/create-input.sh
@@ -1,5 +1,5 @@
 for n in {1..3}; do
-  cmd="dd bs=$((16*1024*1024)) if=/dev/urandom of=file$n.in count=1"
+  cmd="dd bs=$((64*1024)) if=/dev/urandom of=file$n.in count=1"
   echo $cmd
   eval $cmd
 done
