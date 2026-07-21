d51d567ebc2b2dc337715820ccb083e53b7db3fe
Go Iwai <go.iwai@kek.jp>
2026-07-21 15:50:50 +0900

    fix: update B5-array-job.sh to set LSB_JOBID and LSB_JOBINDEX before sourcing Geant4 for enabling running locally and also merged container app execution

--- lsf/geant4/B5-array-job.sh
+++ lsf/geant4/B5-array-job.sh
@@ -1,5 +1,15 @@
 # Usage: bsub -cwd %J/%I -J 'B5[1-10]' -o std.out -e std.err /bin/bash $PWD/B5-array-job.sh
-source /cvmfs/geant4.cern.ch/geant4/latest-ref/x86_64-el9-gcc11-optdeb/bin/geant4.sh
+LSB_JOBID=${LSB_JOBID:-0}
+LSB_JOBINDEX=${LSB_JOBINDEX:-0}
+
+if test -d /.singularity.d; then
+  cd /workspace
+  source /cvmfs/geant4.cern.ch/geant4/latest-ref/x86_64-centos7-gcc10-optdeb/bin/geant4.sh
+  source /cvmfs/sft.cern.ch/lcg/contrib/gcc/10/x86_64-centos7/setup.sh
+  export PATH=/cvmfs/sft.cern.ch/lcg/contrib/CMake/latest/Linux-x86_64/bin:$PATH
+else
+  source /cvmfs/geant4.cern.ch/geant4/latest-ref/x86_64-el9-gcc11-optdeb/bin/geant4.sh
+fi
 cmake $(geant4-config --prefix)/share/Geant4/examples/basic/B5
 make
 
